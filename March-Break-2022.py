714221935b5842877825d52333ea94d7042d19e2
Xwill <89924712+xwillxu@users.noreply.github.com>
2022-03-14 10:10:48 -0400

    Update March-Break-2022.py

--- March-Break-2022.py
+++ March-Break-2022.py
@@ -1,4 +1,4 @@
 # Monday
 """
 Since I Have No School I Don't Really Need To Anything Beside Coding, Reading Books, And Creating Videos.
-But I Have To Go Swimming So I Can Into Competitive Swimming
+But I Have To Go Swimming So I Can Into Competitive Swimming Club. Anywhay March Break Is Basically Going To Be A Very Long Weekend.
